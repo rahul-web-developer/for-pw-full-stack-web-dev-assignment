6f696dc70d349682eeafc77fd302d6d4f417e0d2
Rahul kumar <rahul.webdeveloper.com@gmail.com>
2023-07-26 16:09:09 +0530

    Tailwind css main

---
+++ 4-Tailwind-css-main-solve-here/9-week-tailwind-start/tailwindcss-project/src copy/tailwind.config copy.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme:{
+    extend:{
+      fontsize:{
+        sm:"0.75rem",  //"12px"
+        base:".875rem", //"14px"
+        md:"1rem",      //"16px"
+        lg:"1.25rem",   //"20px"
+                      
+        xl:"1.5rem",     //"24px"
+        "2xl":"1.75rem", //"28px"
+        "3xl":"2.25rem", //"36px"
+      }
+    },
+  },
+  plugins: [],
+}
+
